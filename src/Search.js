f5328943c61a00a8a572dbb5caa1141d89788440
Krzysztof Kozak <hithard5@gmail.com>
2020-06-30 15:22:07 +0200

    added API fetch

--- src/Search.js
+++ src/Search.js
@@ -8,6 +8,35 @@ const Search = () => {
   const [pages, setPages] = useState({});
   const [error, setError] = useState(null);
 
+  const fetchPosts = async (prevPage = null, nextPage = null, count = 0) => {
+    const response = await fetch(
+      `https://www.reddit.com/r/${searchQuery}/new.json?limit=${postLimit}&count=${count}${nextPage ? `&after=${nextPage}` : ''}${prevPage ? `&before=${prevPage}` : ''}`
+    );
+
+    if (response.status === 404) {
+      console.log(response);
+      setError(`I couldn't find this subreddit: https://www.reddit.com/r/${searchQuery}`);
+      setPosts(false);
+    } else {
+      const responseJson = await response.json();
+      setError(null);
+
+      parseResponses(responseJson);
+      setPagination(responseJson);
+    }
+  };
+
+  const parseResponses = (responseJson) => {
+    const posts = responseJson.data.children.map((child) => {
+      return { title: child.data.title, url: child.data.url };
+    });
+    setPosts(posts);
+  };
+
+  const setPagination = (responseJson) => {
+    setPages({ before: responseJson.data.before, after: responseJson.data.after });
+  };
+
   return (
     <>
       <form>
