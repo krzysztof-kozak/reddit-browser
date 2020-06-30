4e576d5a976863ff83863585e9081aca4a93109b
Krzysztof Kozak <hithard5@gmail.com>
2020-06-30 15:32:39 +0200

    added logic to the PostList component, passed props down to the Post component

--- src/PostList.js
+++ src/PostList.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import Post from './Post';
 
-const PostList = () => {
-  return (
-    <>
-      <ul>
-        <Post />
-        <Post />
-      </ul>
-      <button>Prev Page</button>
-      <button>Next Page</button>
-    </>
-  );
+const PostList = ({ posts, pages, fetchPosts }) => {
+  const handleClick = (e) => {
+    if (typeof fetchPosts === 'function') {
+      switch (e.target.value) {
+        case 'prev':
+          fetchPosts(pages.before, null, 0);
+          console.log(pages.before);
+          break;
+        case 'next':
+          fetchPosts(null, pages.after, posts.length);
+          break;
+
+        default:
+          return;
+      }
+    }
+  };
+
+  if (posts) {
+    return (
+      <>
+        <ul>
+          {posts.map((post) => (
+            <Post title={post.title} url={post.url} />
+          ))}
+        </ul>
+        <button value='prev' onClick={handleClick}>
+          Prev Page
+        </button>
+        <button value='next' onClick={handleClick}>
+          Next Page
+        </button>
+      </>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default PostList;
