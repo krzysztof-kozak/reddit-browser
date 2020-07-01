f4c4584ea930159be4aca1f543415addb99293f9
Krzysztof Kozak <hithard5@gmail.com>
2020-07-01 15:04:11 +0200

    Updates

---
+++ precache-manifest.278a3dbfb9a00c13b59fa0ed0617d4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587e384c6038b97782afc336f7be2f12",
+    "url": "/krzysztof-kozak/reddit-browser/index.html"
+  },
+  {
+    "revision": "075752522b736687804f",
+    "url": "/krzysztof-kozak/reddit-browser/static/css/main.8c34f0de.chunk.css"
+  },
+  {
+    "revision": "b576840d798794b2a23d",
+    "url": "/krzysztof-kozak/reddit-browser/static/js/2.84273b5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/krzysztof-kozak/reddit-browser/static/js/2.84273b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075752522b736687804f",
+    "url": "/krzysztof-kozak/reddit-browser/static/js/main.d29cb8d5.chunk.js"
+  },
+  {
+    "revision": "53665615faba83ba593b",
+    "url": "/krzysztof-kozak/reddit-browser/static/js/runtime-main.c2bb2023.js"
+  }
+]);
