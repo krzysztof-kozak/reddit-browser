308dc78bfea5602fb26d6b7db599489639f186a2
Krzysztof Kozak <hithard5@gmail.com>
2020-07-01 15:14:43 +0200

    Updates

---
+++ precache-manifest.aa3f996ffda89f3036c63f56adb30b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc688fa8878ba96c7ec4646a27149911",
+    "url": "/reddit-browser/index.html"
+  },
+  {
+    "revision": "f02644f0b114fb82810f",
+    "url": "/reddit-browser/static/css/main.8c34f0de.chunk.css"
+  },
+  {
+    "revision": "b576840d798794b2a23d",
+    "url": "/reddit-browser/static/js/2.84273b5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reddit-browser/static/js/2.84273b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02644f0b114fb82810f",
+    "url": "/reddit-browser/static/js/main.7be5a275.chunk.js"
+  },
+  {
+    "revision": "f0d54a51f269662be653",
+    "url": "/reddit-browser/static/js/runtime-main.24718a61.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/krzysztof-kozak/reddit-browser/precache-manifest.278a3dbfb9a00c13b59fa0ed0617d4f4.js"
+  "/reddit-browser/precache-manifest.aa3f996ffda89f3036c63f56adb30b19.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/krzysztof-kozak/reddit-browser/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reddit-browser/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
