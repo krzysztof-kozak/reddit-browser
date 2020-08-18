f52720ab0071793ca434f8060ad24129050aede0
Krzysztof Kozak <hithard5@gmail.com>
2020-08-18 22:26:04 +0200

    Improve search logic

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import PostList from './PostList';
+import React, { useState } from "react";
+import PostList from "./PostList";
 
 const Search = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [postLimit, setPostLimit] = useState(10);
-  const [sortBy, setSortBy] = useState('new');
+  const [sortBy, setSortBy] = useState("new");
   const [posts, setPosts] = useState(false);
   const [pages, setPages] = useState({});
   const [error, setError] = useState(null);
   const [numberOfPostsFetched, setNumberOfPostsFetched] = useState(0);
-  const [title, setTitle] = useState('');
-  const [subTitle, setSubTitle] = useState('');
+  const [title, setTitle] = useState("");
+  const [subTitle, setSubTitle] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     resetCount();
-    setSubTitle(e.target.sortBy.value.replace(/\s+/g, '').toLowerCase());
+    setSubTitle(e.target.sortBy.value.replace(/\s+/g, "").toLowerCase());
     setTitle(e.target.search.value);
 
     if (searchQuery) {
@@ -36,29 +36,42 @@ const Search = () => {
   };
 
   const handleNextPage = () => {
-    setNumberOfPostsFetched((prev) => prev + posts.length);
-    fetchPosts(null, pages.after, numberOfPostsFetched);
+    let isRun = true;
+    setNumberOfPostsFetched((prev) => {
+      if (isRun) {
+        isRun = !isRun;
+        fetchPosts(null, pages.after, prev + posts.length);
+      }
+      return prev + posts.length;
+    });
   };
 
   const handlePrevPage = () => {
-    if (numberOfPostsFetched - posts.length >= 0) {
-      setNumberOfPostsFetched((prev) => prev - posts.length);
-    } else {
-      setNumberOfPostsFetched(0);
-    }
-    fetchPosts(pages.before, null, numberOfPostsFetched);
+    let isRun = true;
+    setNumberOfPostsFetched((prev) => {
+      const currentCount = prev - posts.length > 0 ? prev - posts.length : 0;
+      if (isRun) {
+        isRun = !isRun;
+        fetchPosts(pages.before, null, currentCount);
+      }
+      return currentCount;
+    });
   };
 
   const fetchPosts = async (prevPage = null, nextPage = null, count = 0) => {
-    if (count === 0) {
-      prevPage = null;
-    }
+    // if (count === 0) {
+    //   prevPage = null;
+    // }
     const response = await fetch(
-      `https://www.reddit.com/r/${searchQuery}/${sortBy}.json?limit=${postLimit}&count=${count}${nextPage ? `&after=${nextPage}` : ''}${prevPage ? `&before=${prevPage}` : ''}`
+      `https://www.reddit.com/r/${searchQuery}/${sortBy}.json?limit=${postLimit}&count=${count}${
+        nextPage ? `&after=${nextPage}` : ""
+      }${prevPage ? `&before=${prevPage}` : ""}`
     );
 
     if (response.status === 404) {
-      setError(`I couldn't find this subreddit: https://www.reddit.com/r/${searchQuery}`);
+      setError(
+        `I couldn't find this subreddit: https://www.reddit.com/r/${searchQuery}`
+      );
       setPosts(false);
     } else {
       const responseJson = await response.json();
@@ -75,61 +88,92 @@ const Search = () => {
       //Sticked posts do not adhere to the post per page limit. Filtering them out is not ideal, but I couldn't find a better solution.
       // .filter((post) => post.data.stickied === false)
       .map((child) => {
-        return { title: child.data.title, url: child.data.permalink, subredditName: child.data.subreddit_name_prefixed, author: child.data.author, key: child.data.id };
+        return {
+          title: child.data.title,
+          url: child.data.permalink,
+          subredditName: child.data.subreddit_name_prefixed,
+          author: child.data.author,
+          key: child.data.id,
+        };
       });
     setPosts(posts);
   };
 
   const setPagination = (responseJson) => {
-    setPages({ before: responseJson.data.before, after: responseJson.data.after });
+    setPages({
+      before: responseJson.data.before,
+      after: responseJson.data.after,
+    });
   };
 
   return (
     <>
-      <form className='form' onSubmit={handleSubmit}>
-        <h1 className='form__title'>Reddit App</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <h1 className="form__title">Reddit App</h1>
 
-        <p className='search-wrapper'>
-          <label className='form__label form__label--main'>reddit/r</label>
-          <input className='form__input' id='search' placeholder='e.g reactjs' onChange={(e) => setSearchQuery(e.target.value.replace(/\s+/g, ''))}></input>
+        <p className="search-wrapper">
+          <label className="form__label form__label--main">reddit/r</label>
+          <input
+            className="form__input"
+            id="search"
+            placeholder="e.g reactjs"
+            onChange={(e) => setSearchQuery(e.target.value.replace(/\s+/g, ""))}
+          ></input>
         </p>
 
-        <p className='search-wrapper'>
-          <label className='form__label' htmlFor='postLimit'>
+        <p className="search-wrapper">
+          <label className="form__label" htmlFor="postLimit">
             posts per page
           </label>
-          <select className='form__select' id='postLimit' name='postLimit' onChange={handlePostLimitChange}>
+          <select
+            className="form__select"
+            id="postLimit"
+            name="postLimit"
+            onChange={handlePostLimitChange}
+          >
             <option value={10}>10</option>
             <option value={20}>20</option>
             <option value={30}>30</option>
           </select>
         </p>
 
-        <p className='search-wrapper'>
-          <label className='form__label' htmlFor='sortBy'>
+        <p className="search-wrapper">
+          <label className="form__label" htmlFor="sortBy">
             sort posts by
           </label>
-          <select className='form__select' id='sortBy' name='sortBy' onChange={handleSortByChange} value={sortBy}>
-            <option value='new'>new</option>
-            <option value='top'>top</option>
-            <option value='hot'>hot</option>
-            <option value='rising'>rising</option>
+          <select
+            className="form__select"
+            id="sortBy"
+            name="sortBy"
+            onChange={handleSortByChange}
+            value={sortBy}
+          >
+            <option value="new">new</option>
+            <option value="top">top</option>
+            <option value="hot">hot</option>
+            <option value="rising">rising</option>
           </select>
         </p>
 
-        <button className='form__button' type='submit'>
+        <button className="form__button" type="submit">
           search
         </button>
 
-        {error ? <p className='error'>{error}</p> : null}
+        {error ? <p className="error">{error}</p> : null}
       </form>
 
       <PostList posts={posts} title={title} subtitle={subTitle} />
-      <div className='button-wrapper'>
-        <button className={posts ? 'list__button' : 'hidden'} onClick={handlePrevPage}>
+      <div className="button-wrapper">
+        <button
+          className={posts ? "list__button" : "hidden"}
+          onClick={handlePrevPage}
+        >
           Prev Page
         </button>
-        <button className={posts ? 'list__button' : 'hidden'} onClick={handleNextPage}>
+        <button
+          className={posts ? "list__button" : "hidden"}
+          onClick={handleNextPage}
+        >
           Next Page
         </button>
       </div>
